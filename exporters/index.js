7c003f208290c4d044ba6a754368ce38de2eb1ed
Tim Raveling <tsraveling@gmail.com>
2025-02-24 13:34:06 -0700

    Removed log

--- exporters/index.js
+++ exporters/index.js
@@ -144,7 +144,6 @@ function prepareData(data, options) {
 
 function startExporter(exporter, data, options) {
   let { rects, config } = prepareData(data, options);
-  console.log(">>> rects:", rects)
   let renderOptions = {
     rects: rects,
     config: config,
